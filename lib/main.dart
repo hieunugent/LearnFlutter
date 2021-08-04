dd8b6073affa658aea8c84a85b32a3fff5c9de4d
hieunugent <hieunugent@gmail.com>
2021-08-04 00:06:37 -0700

    add qquestion.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
     var question = [
       'What\'s your favorite color',
@@ -17,18 +35,20 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Question(
+              question[_questionIndex],
+            ),
             ElevatedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: () => {print('choose answer 2')},
             ),
             ElevatedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: () => {print('choose answer 2')},
             ),
           ],
         ),
---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  // const Question({ Key? key }) : super(key: key);
+  String questionText;
+  Question(this.questionText);
+  @override
+  Widget build(BuildContext context) {
+    return Text(questionText);
+  }
+}
